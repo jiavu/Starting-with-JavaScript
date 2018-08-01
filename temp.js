e6ba5fc8a4ffc84656bf9af30ec486da7e9c7ab3
jiavu <cs@CS-Germany-iMac.local>
2018-08-01 21:37:30 +0200

    nichts Wichtiges

--- temp.js
+++ temp.js
@@ -3,8 +3,12 @@
 let object = {
     key : "Value"
 };
+object.key = "New Value";
 
-object.key = "asldkj";
+let randomNumber = Math.floor(Math.random() * 10000);
 
 document.getElementById("gime-js1").innerHTML = object.key;
 document.getElementById("gime-js2").innerHTML = object;
+document.getElementById("gime-js3").innerHTML = "Random PIN: " + randomNumber;
+
+alert("I'm a JavaScript!");
