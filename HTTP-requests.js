97e75d4b0132f26844e51d104e3242f96f949d32
jiavu <39624614+jiavu@users.noreply.github.com>
2018-08-12 22:10:33 +0200

    Continued, added
    Boilerplate code for an async GET request (ES8) using await

--- HTTP-requests.js
+++ HTTP-requests.js
@@ -203,6 +203,39 @@ fetch(url, {// settings object //
 
 
 
-///////////////////////////////////
-// III. async GET requests (ES8) //
-///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+// III. Boilderplate code for an async GET request (ES8) (using await) //
+/////////////////////////////////////////////////////////////////////////
+
+const getData = async() => {
+    try {
+        const response = await fetch("https://api-to-call.com/endpoint");       // sends request
+        if (response.ok) {                                              //
+            const jsonResponse = await response.json();                 // handles response if successful
+            return jsonResponse;                                        //
+        }                                                               //
+        throw new Error("Request failed!");     //
+    }                                           // handles response if unsuccessful
+    catch(error) { console.log(error) };        //
+};
+
+// Description:
+
+async function getData() {          // the async keyword will ensure that the function returns a promise.
+    try {                       // (send request, handle response)
+        const response = await fetch(url);  // await can only be used in an async function. await allows a program to run
+                                            // while waiting for a promise to resolve.
+        if (response.ok) {
+            const jsonResponse = await response.json(); // Since .json() is an asynchronous method we have to await
+                                                        // until the promise status is resolved.
+            return jsonResponse;            // and/or code to execute with jsonResponse.
+        }
+    throw new Error("Request failed!");
+    }
+    catch (error) {
+    console.log(error)  // Code block for exception handling, e. g. redirect to another page.
+    }
+}
+
+/* in a try...catch statement, code in the try block will be run and in the event of an excpetion/error, the code in the catch
+statement will run. */
